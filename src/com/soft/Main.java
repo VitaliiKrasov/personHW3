69bc3b0ee4db1fed9d8ae1e6620ecad80010f14e
VitaliiKrasov <krasovvitalii@gmail.com>
2018-11-10 21:32:02 +0200

    added task description

--- src/com/soft/Main.java
+++ src/com/soft/Main.java
@@ -1,3 +1,18 @@
+/**
+ * Create Console Application project in Java.
+ * Add class Person to the project.
+ * Class Person should consists of
+ * two private fields: name and birthYear (the birthday year)
+ * properties for access to these fields
+ * default constructor and constructor with 2 parameters
+ * methods:
+ * age - to calculate the age of person
+ * input - to input information about person
+ * output - to output information about person
+ * changeName - to change the name of person
+ * In the method main() create 5 objects of Person type and input information about them
+ */
+
 package com.soft;
 
 import java.io.IOException;
--- src/com/soft/PersonTest.java
+++ src/com/soft/PersonTest.java
@@ -4,15 +4,16 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Calendar;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class PersonTest {
-
-    Person p = new Person("", 2000);
+//  Given
+    Person p = new Person("Liolik", 2000);
     int currentYear = Calendar.getInstance().get(Calendar.YEAR);
+//  When
     int actual = p.age();
     int expected = currentYear - 2000;
-
+//  Then
     @Test
     void age() {
         assertEquals(expected, actual);
